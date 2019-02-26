991a062ad141258ac1b8fc21b773bf639db5b257
Kyle <locrian20012001@yahoo.com>
2019-02-26 00:20:10 -0500

    Adding to Sarah as I explore how NPC's work.

--- stories/cauldron/zones/sarah.py
+++ stories/cauldron/zones/sarah.py
@@ -8,4 +8,15 @@ from tale.verbdefs import AGGRESSIVE_VERBS
 class Sarah(Living):
     def init(self) -> None:
         super().init()
+        self.verbs['ask'] = "Ask Sarah a question"
+
+    def handle_verb(self, parsed, actor):
+        if parsed.verb == "ask":
+            actor.tell(parsed)
+            actor.tell('')
+
+            if 'herself' in parsed.args:
+                actor.tell("Ah, how nice. You'd like to know about . . . me.")
+
+        return True
 
